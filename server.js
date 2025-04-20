654ab65ff988ce27eeec7698ccc11616d69c4f5a
Zunair <zunair.sarwar@upgenicsint.work>
2025-04-20 18:47:40 +0500

    - Change port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const path = require('path');
-const app = require('./app');
+const app = express();
 dotenv.config();
 
 // Middleware
