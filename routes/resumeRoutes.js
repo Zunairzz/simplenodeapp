e153f8db75f9ac18b3445962982f267ea7aa1dd3
Zunair <zunair.sarwar@upgenicsint.work>
2025-05-11 21:59:10 +0500

    - Remove multer

--- routes/resumeRoutes.js
+++ routes/resumeRoutes.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const multer = require('multer');
-const path = require('path');
 const {v2: cloudinary} = require('cloudinary');
 const {ENDPOINTS} = require('../util/Constants');
 const resumeController = require("../controllers/resumeController");
@@ -13,20 +11,11 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-// Multer setup
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, 'uploads/'),
-    filename: (req, file, cb) => cb(null, Date.now() + path.extname(file.originalname))
-});
-const upload = multer({storage});
-
 router.post(ENDPOINTS.RESUME.ADD, resumeController.createResume);
 router.get(ENDPOINTS.RESUME.GET_ALL, resumeController.getAllResumes);
 router.get(ENDPOINTS.RESUME.GET_BY_ID, resumeController.getResumeById);
 router.put(ENDPOINTS.RESUME.UPDATE, resumeController.updateResumeById);
 router.delete(ENDPOINTS.RESUME.DELETE, resumeController.deleteResumeById);
-router.post('/uploads', upload.single('resume'), resumeController.uploadPdf);
-router.put('/update-pdf', resumeController.updatePdf);
 router.delete(ENDPOINTS.RESUME.DELETE_RESOURCES, resumeController.deleteResources)
 
 module.exports = router;
