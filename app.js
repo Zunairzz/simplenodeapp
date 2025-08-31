9fe467c716a4d9a30a66649703a2fb506c285b15
Zunair <zunair.sarwar@upgenicsint.work>
2025-09-01 01:08:54 +0500

    - Change user routes for CommonJS to ESM

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const dotenv = require('dotenv');
 const {urlencoded} = require('body-parser');
 const app = express();
-const {BASE_URLS, PROBLEM_BASE_URL, RESUME_BASE_URL, PROJECT_BASE_URL} = require('./util/Constants');
+import {Constants} from "./util/Constants.js";
+
+const {BASE_URLS, PROBLEM_BASE_URL, RESUME_BASE_URL, PROJECT_BASE_URL} = Constants;
 
 const userRoutes = require('./routes/userRoutes'); // Ensure correct path
 const projectRoutes = require('./routes/projectRoutes'); // Ensure correct path
@@ -23,22 +25,22 @@ app.use(express.json());
 console.log('Express JSON parser middleware configured.');
 
 // Routes
-app.use('/api/users', (req, res, next) => {
+app.use(BASE_URLS.USER, (req, res, next) => {
     console.log(`[${new Date().toISOString()}] /api/users route accessed`);
     next();
 }, userRoutes);
 
-app.use('/api/project', (req, res, next) => {
+app.use(BASE_URLS.PROJECT, (req, res, next) => {
     console.log(`[${new Date().toISOString()}] /api/project route accessed`);
     next();
 }, projectRoutes);
 
-app.use('/api/resume', (req, res, next) => {
+app.use(BASE_URLS.RESUME, (req, res, next) => {
     console.log(`[${new Date().toISOString()}] /api/resume route accessed`);
     next();
 }, resumeRoutes);
 
-app.use('/api/problem', (req, res, next) => {
+app.use(BASE_URLS.PROBLEM, (req, res, next) => {
     console.log(`[${new Date().toISOString()}] /api/problem route accessed`);
     next();
 }, problemsRoutes);
